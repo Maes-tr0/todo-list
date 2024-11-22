a1b758e3dfc0c835daaa27ee8ae6344f32be5d6c
Maes-tr0 <nikita.opara.work@gmail.com>
2024-11-22 15:17:16 +0200

    add generate unique Id

--- src/main/java/ua/maestr0/todolist/service/NoteService.java
+++ src/main/java/ua/maestr0/todolist/service/NoteService.java
@@ -1,40 +1,60 @@
 package ua.maestr0.todolist.service;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ua.maestr0.todolist.model.Note;
 
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 @Service
 public class NoteService {
-
     private final Map<Long, Note> notes;
+    private final Random idGenerator;
 
-    public NoteService(){
+    public NoteService() {
         notes = new LinkedHashMap<>();
+        idGenerator = new Random();
     }
 
-    public List<Note> listAll(){
+    public List<Note> listAll() {
         return notes.values().stream().toList();
     }
 
-    public Note add(Note note){
+    public Note add(Note note) {
+        note.setId(generateUniqueId());
         return notes.put(note.getId(), note);
     }
 
-    public void deleteById(long id){
+    public void deleteById(long id) {
+        isExistId(id);
         notes.remove(id);
     }
 
-    public Note getById(long id){
+    public Note getById(long id) {
+        isExistId(id);
         return notes.get(id);
     }
 
-    public void update(Note note){
-        notes.put(note.getId(), note);
+    public void update(Note note) {
+        Long id = note.getId();
+        isExistId(id);
+        notes.put(id, note);
+    }
+
+    private void isExistId(Long id) {
+        if (notes.containsKey(id)) {
+            throw new IllegalArgumentException("Note id " + id + " not exists");
+        }
+    }
+
+    private Long generateUniqueId() {
+        Long id;
+        do {
+            id = (long) (idGenerator.nextInt(Integer.MAX_VALUE) + 1); // Уникальний ID (позитивний)
+        } while (notes.containsKey(id));
+        return id;
     }
 }
