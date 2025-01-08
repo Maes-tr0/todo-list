c147301531bd767527e12a53e0e3550716416977
Maes-tr0 <nikita.opara.work@gmail.com>
2025-01-08 19:20:05 +0200

    Add Spring Security

--- src/main/java/ua/maestr0/todolist/TodoListApplication.java
+++ src/main/java/ua/maestr0/todolist/TodoListApplication.java
@@ -1,12 +1,9 @@
 package ua.maestr0.todolist;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class TodoListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
